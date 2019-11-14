b6b0cc86afd815c929cd4b3bd014fd4d2beda067
Michel Richter <michel.richter@outlook.de>
2019-11-14 14:46:52 +0100

    version4

--- michel/todo-app/src/version3/Rating.js
+++ michel/todo-app/src/version3/Rating.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default function Rating(props)
 {
     return(
-        
+        <div>{
             "*".repeat(props.NumberOfStars)
-        );
+            }</div>
+    );
 }
---
+++ michel/todo-app/src/versionX/ToDo.js
@@ -0,0 +1,10 @@
+export default class ToDo {
+    constructor(id, title, completed, rating, userName)
+    {
+        this.id = id;
+        this.title = title;
+        this.completed = completed;
+        this.rating = rating;
+        this.userName = userName;
+    }
+}
