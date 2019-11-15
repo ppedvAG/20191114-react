e7316ab24ad2c74f73225b9734e3dda2e4b57718
Vadzim Naumchyk <vadzimn@ppedv.de>
2019-11-15 09:42:41 +0100

    lifting state up schema

---
+++ vadzim/theory-app/src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './module10/index.css';
+// ohne Namensänderung
+// import { Banana } from './App';
+// mit Namensänderung
+// import * as serviceWorker from './serviceWorker';
+
+// M10 | GETTING STARTED
+// import App from './module10/App';
+
+// 2. jsx intro
+// import App from './module20/App';
+
+// 3. function components and props
+// import App from './module30/App';
+
+// 4. jsx addition
+// import App from './module40/App';
+
+// 5. class components and state
+// import App from './module50/App';
+
+// 6. react & forms
+// import App from './module60/App';
+
+// 91. Lifting state up
+// import App from './module91/App';
+// import App from './module92/App';
+// import App from './module93/App';
+// import App from './module94/AppWithClass';
+// import App from './module94/AppWithFunctions';
+
+// 7. Lifecycle methods
+// nehmen sie Inhalt für diese Datei aus module70/indexM7.js
+
+// 8. 
+import App from './moduleCurrent/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
